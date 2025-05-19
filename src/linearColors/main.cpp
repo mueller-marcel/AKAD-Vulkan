103b3140b51f3f12d0991d4bf87e45ef8af04bab
Marcel Müller <mueller.marcel69@gmail.com>
2025-05-19 20:59:50 +0200

    Initial commit

---
+++ src/linearColors/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+
+    // Check whether glfw is able to initialize
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Linear Colors", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to open GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
